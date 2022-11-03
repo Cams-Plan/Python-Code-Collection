887282a0e3b20d67cca63e4a632e292b7b6e7e0c
Camille Francis <110306796+Cams-Plan@users.noreply.github.com>
2022-11-03 21:39:57 +0000

    Create BMI Calculator Upgrade.py

---
+++ BMI Calculator Upgrade.py
@@ -0,0 +1,7 @@
+### UPGRADED DEVELOPMENT OF THE 100 Days of Code BMI CALC
+# Input + Conversion - concise input conversion
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# Calculation
+bmi = weight / (height ** 2)
+print(int(bmi))
