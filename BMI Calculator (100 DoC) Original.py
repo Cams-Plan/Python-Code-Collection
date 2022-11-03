e67b3492c62670502f72c20ce31c3551d297f8d7
Camille Francis <110306796+Cams-Plan@users.noreply.github.com>
2022-11-03 21:22:03 +0000

    Create BMI Calculator (100 DoC) Original.py

---
+++ BMI Calculator (100 DoC) Original.py
@@ -0,0 +1,23 @@
+### LONG CODE ###
+# # 🚨 Don't change the code below 👇
+# height = input("enter your height in m: ")
+# weight = input("enter your weight in kg: ")
+# # 🚨 Don't change the code above 👆
+
+# #Write your code below this line 👇
+# weight = float(weight)
+# height = float(height)
+# bmi = weight / (height ** 2 * (height ** 2))
+# print(round(bmi,2))
+
+### SHORT CODE ###
+
+# Data Input Variables
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# Type Conversions
+weight = float(weight)
+height = float(height)
+# Calculation
+bmi = weight / (height ** 2 * (height ** 2))
+print(round(bmi,2))
