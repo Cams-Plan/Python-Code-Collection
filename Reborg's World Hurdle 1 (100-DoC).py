299528d132ca18ff56f551d17883abaae25d7e48
Camille Francis <110306796+Cams-Plan@users.noreply.github.com>
2022-11-15 17:16:33 +0000

    Create Reborg's World Hurdle 1 (100-DoC).py

---
+++ Reborg's World Hurdle 1 (100-DoC).py
@@ -0,0 +1,37 @@
+### CLICK THE LINK BELOW to the website to see how the code works ###
+## https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+## It's a great way to practice creating functions and understanding flow
+
+def single_jump_right():
+    #Function that used 4 tiles to jump over 1 tile hurdle
+    #Ascension
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    #landing
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+#Start of the hurdle journey
+move()
+single_jump_right()
+turn_left()
+move()
+single_jump_right()
+turn_left()
+move()
+single_jump_right()
+turn_left()
+move()
+single_jump_right()
+turn_left()
+move()
+single_jump_right()
+turn_left()
+move()
+single_jump_right()
+turn_left()
