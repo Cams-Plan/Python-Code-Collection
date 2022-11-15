427b915f41eb520cd72a8f703f8a636da741a118
Camille Francis <110306796+Cams-Plan@users.noreply.github.com>
2022-11-15 20:10:20 +0000

    Update Reeborg's World Hurdle 3 (while + if) (100-DoC) original.py

--- Reeborg's World Hurdle 3 (while + if) (100-DoC) original.py
+++ Reeborg's World Hurdle 3 (while + if) (100-DoC) original.py
@@ -1,5 +1,5 @@
 ### COPY AND PASTE THE LINK BELOW to go to the Reeborg's World Hurdle 3 level and try the code ###
-## Link:
+## Link: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
 
 def turn_right():
     #Function that turns right within tile
