6de17433c7f2b68f16359adcabe3769621fc36c4
Camille Francis <110306796+Cams-Plan@users.noreply.github.com>
2022-11-25 00:12:09 +0000

    Create ASCII_Art_Hangman.py

---
+++ ASCII_Art_Hangman.py
@@ -0,0 +1,16 @@
+## A streamlined ASCII art for 6-life hangman code to reduce your lines of code. to call 
+# on individual diagrams, use print(diagram[index]) e.g. print(diagram[6]) or a variable
+# with an int value that represents lives within the range of 'diagram' index 
+# values (0-6).
+
+diagram = [
+    "   +---+\n       |\n       |\n       |\n      ===\n",
+    "   +---+\n   O   |\n       |\n       |\n      ===\n",
+    "   +---+\n   O   |\n   |   |\n       |\n      ===\n",
+    "   +---+\n   O   |\n  /|   |\n       |\n      ===\n",
+    "   +---+\n   O   |\n  /|\  |\n       |\n      ===\n",
+    "   +---+\n   O   |\n  /|\  |\n  /    |\n      ===\n",
+    "   +---+\n   O   |\n  /|\  |    [GAME OVER! x_X] \n  / \  |\n      ===\n"
+    ]
+
+#by camille @cams-plan
