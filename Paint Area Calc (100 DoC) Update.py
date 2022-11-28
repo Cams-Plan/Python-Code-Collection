35620b8454b73044804513361df863321f9b5568
Camille Francis <110306796+Cams-Plan@users.noreply.github.com>
2022-11-28 00:38:17 +0000

    Create Paint Area Calc (100 DoC) Update.py

---
+++ Paint Area Calc (100 DoC) Update.py
@@ -0,0 +1,20 @@
+### UPDATE FROM ORIGINAL Paint Area Calc. - incorporates 'math' module to ensure correct rounding of numbers
+
+## Create a function that calculates the required amount of paint, based on parameters given (area to be painted)
+import math
+
+def paint_calc(height, width, cover):
+    #calculates number cans needed to finish paint job
+    area = height * width
+    can_calc = math.ceil(area / cover)
+    if can_calc == 1:
+        print(f"You'll need {can_calc} can of paint")
+    else:
+        print(f"You'll need {can_calc} cans of paint")
+  
+
+# 🚨 Don't change the code below 👇         ## PRE-MADE CODE BELOW
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
