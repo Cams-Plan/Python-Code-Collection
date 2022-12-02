e0c4e186f4f823c2f5932d2ef88b8668211cb8f8
Camille Francis <110306796+Cams-Plan@users.noreply.github.com>
2022-12-02 19:56:01 +0000

    Update and rename Day 8 Project - Ceasar Cipher. Original.py to Day 8 Project - Caesar Cipher. Original.py

--- Day 8 Project - Caesar Cipher. Original.py
+++ Day 8 Project - Caesar Cipher. Original.py
@@ -1,4 +1,4 @@
-## Create a Ceasar Cipher that encrypts and decrypts any message
+## Create a Caesar Cipher that encrypts and decrypts any message
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k','l'
 , 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
