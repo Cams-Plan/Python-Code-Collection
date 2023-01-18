f971e77f2acd362056a24e48db6bc1b55d113923
Camille Francis <110306796+Cams-Plan@users.noreply.github.com>
2023-01-18 02:35:04 +0000

    Update Dictionary in list exercise D8 (100DoC).py

--- Dictionary in list exercise D8 (100DoC).py
+++ Dictionary in list exercise D8 (100DoC).py
@@ -16,10 +16,7 @@ travel_log = [
 
 # 1 Write the function that will allow new countries #to be added to the travel_log.
 def add_new_country(thecountry, thevisit, thecities):
-    new = {"country": "none", 
-    "visits": "none", 
-    "cities": "none"
-    }
+    new = {}
     new.update({"country": thecountry, "visits": thevisit, "cities": thecities})
     travel_log.append(new)
 
@@ -30,10 +27,8 @@ print(travel_log)
 
             ## COMMENTS/DECISIONS
             ## 1
-            ## I decided to make template dictionary called new to transfer the function parameter data
+            ## I made an empty dictionary called new to transfer the function parameter data
             # into so it would be in format for appending later.
-            ## the values couldn't be blank, so I chose "none" as the value as it made the most contextual 
-            # sense, but it could've been any placeholder.
             ## I used .update() to change multiple values of the dictionary at once, to save lines of code.
             ## Once all changes were made, I just needed to append the data from new to into the list
             # which would collect the full dictionary as one list item.
