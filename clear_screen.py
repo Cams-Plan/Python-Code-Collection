0c8904fa295e53e1bafce1b5d41333f23c9c3c21
Camille Francis <110306796+Cams-Plan@users.noreply.github.com>
2023-04-07 22:24:00 +0100

    Create clear_screen.py

---
+++ clear_screen.py
@@ -0,0 +1,8 @@
+import os
+from time import sleep
+
+def clear_terminal():
+    #Pause to allow reading of preceding short text (if applicable)
+    sleep(2)
+    #Clear screen
+    os.system('cls')
